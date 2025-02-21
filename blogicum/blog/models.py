3ea1c86c0918af649c26141d6ba0e38576810c1c
Elizaveta Babenkova <liz2405@mail.ru>
2025-02-22 01:15:12 +0300

    smvd

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -52,8 +52,8 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
         help_text='Если установить дату и \
-            время в будущем — можно делать 
-отложенные публикации.'
+            время в будущем — можно делать \
+                отложенные публикации.'
                                    )
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
